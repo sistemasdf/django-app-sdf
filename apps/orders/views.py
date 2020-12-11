5025cc5ccd243efecd698783140eca2af3d8e63d
Kristofeer Moran Guevara <kristofeer@MacBook-Air-de-Kristofeer.local>
2020-12-11 02:00:35 -0500

    Nuevo API de prueba!

--- apps/orders/views.py
+++ apps/orders/views.py
@@ -18,6 +18,36 @@ class OrderViewSet(viewsets.ModelViewSet):
     parser_classes = (MultiPartParser, FormParser)
 
     def create(self, request, *args, **kwargs):
+        print (request.FILES['invoice'])
+        print (request.data['weavers_id'])
+        print (request.data['spinningmills_id'])
+        file_name = request.FILES['invoice'].name
+        extension = file_name.split(".")
+        orderid = 0;
+        while True:
+            request.FILES['invoice'].name = '{0}{1}{2}{3}{4}.{5}'.format("invoice",random.choice("AEIOU"),random.choice("AEIOU"),random.randrange(1000),random.randrange(1000),extension[len(extension)-1])
+            #pruebaname = '{0}{1}{2}{3}{4}.{5}'.format("invoice",random.choice("AEIOU"),random.choice("AEIOU"),random.randrange(1000),random.randrange(1000),".pdf")
+            order_list = Order.objects.filter(invoice_name=request.FILES['invoice'].name).values("order_id")
+            if not order_list:
+                weavers = Weavers.objects.get(weavers_id=request.data['weavers_id'])
+                spinningmills = SpinningMills.objects.get(spinningmills_id=request.data['spinningmills_id'])
+                order = Order()
+                order.weavers = weavers
+                order.spinningmills = spinningmills
+                order.invoice = request.FILES['invoice']
+                order.invoice_name = request.FILES['invoice'].name
+                #order.invoice_name = pruebaname
+                order.save()
+                orderid = order.order_id
+                break
+        message_response = {
+            "success": "Se guardo correctamente",
+            "order_id": orderid
+        }
+        return Response(message_response, status=status.HTTP_201_CREATED)
+
+    @action(detail=False, methods=['post'], url_path='order-test')
+    def ordertest(self, request):
         #print (request.FILES['invoice'])
         print (request.data['weavers_id'])
         print (request.data['spinningmills_id'])
